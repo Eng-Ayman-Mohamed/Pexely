2c24e991c9727b700fda57a80df8152f466cd25b
Ayman-Mohamed-abotaha <eng.ayman.mohamed.2001@gmail.com>
2024-02-26 15:38:23 +0200

    nav bar styling

--- src/App.js
+++ src/App.js
@@ -32,24 +32,24 @@ function App() {
     <div className={mode ? "dark bg-slate-900 " : "  "}>
       <nav
         className="fixed w-lvw flex backdrop-blur
-       justify-center   mb-5 p-4  
-       shadow-lg  lg:text-3xl md:text-2xl sm:text-xl"
+       justify-center  p-4 vsm:p-1 h-[10vh] vsm:h-14
+       shadow-lg  lg:text-3xl md:text-2xl items-center "
       >
         <input
           type="text"
           className=" mt-1 block w-[50vw]  px-3 py-2 bg-inherit 
-           border-slate-300 border-2 rounded-3xl shadow-sm
+           border-slate-300 border-2 rounded-3xl shadow-sm vsm:h-8
           focus:outline-none focus:border-sky-500 focus:ring-1
-           focus:ring-sky-500 dark:text-purple-700 text-center font-serif 
-           text-lg
+           focus:ring-sky-500 dark:text-purple-700 text-center
+            font-serif text-lg sm:text-sm 
        "
           onKeyDown={handleChange}
         ></input>
         <button
           onClick={Dark}
-          className="absolute   right-9 text-lg size-15 
+          className=" text-lg size-15 top-2
           font-bold px-3 py-2 text-slate-700 rounded-lg 
-            dark:text-white "
+            dark:text-white  "
         >
           {mode ? <MdOutlineLightMode /> : <MdDarkMode />}
         </button>
@@ -61,7 +61,7 @@ function App() {
           return (
             <img
               key={animal.id}
-              className="h-auto w-[80vw] hover:cursor-pointer hover:w-[79vw]"
+              className="h-auto w-[90vw] hover:cursor-pointer "
               src={animal.src.original}
               alt=""
             />
